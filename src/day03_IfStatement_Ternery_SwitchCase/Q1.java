23c07503adae9fef0abb67cab9cec5ef4471b779
seyfullah merdan <merdanseyfullah33@gmail.com>
2021-12-03 20:08:44 +0100

    javaPractice

---
+++ src/day03_IfStatement_Ternery_SwitchCase/Q1.java
@@ -0,0 +1,58 @@
+package day03_IfStatement_Ternery_SwitchCase;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+	
+		/*
+        1.Kullanýcýya    "Vize sýnav sonucunuzu giriniz :"
+        2.vize notu double deðerinde girilecek.
+
+        3. Kullanýcýya  "Vize sýnavý yüzdesini giriniz". ->> double olmalý ->> 0,40 etkiliyor   !!Yüzde kaç etkilediðini Sisteme siz giriyorsunuz.!!
+
+        4.Kullanýcýya    "Final sýnav sonucunuzu giriniz.
+
+        5. final notu double deðerinde olmalý. (örn: 65,5)
+
+        6.Kullanýcýya    "Final sýnavý yüzdesini giriniz"-->> double olmalý ->> 0,60 etkiliyor.
+
+        7. Vize final ortalamasýný bulmak için, vize nin yüzde40 ý, finalin yüzde60 ý alýnmalý ve çýkan sonuçlar toplanmalýdýr.
+
+        8. Çýkan sonucu (dersin not ortalamasýný)   double toplam  ' a eþitleyebilirsiniz.
+
+                                        todo    Koþul kýsmý
+
+                                        eðer ortalamasý 90(90 dahil) 'dan büyük ise, harf notu olarak AA,
+                                                                        80(80dahil)   ile 90 arasýnda ise  BA,
+                                                                        70(70 dahil)   ile 80 arasýnda ise BB,
+                                                                        60(60dahil) ile 70 arasýnda ise CB,
+                                                                        50(50 dahil) ile 60 arasýnda ise CC,
+                                                                     40(40 dahil) ile 50 arasýnda ise DC,
+                                                                     30(30 dahil) ile 40 arasýnda ise DD,
+                                                                     30 'dan düþük ise FF            gelmeli.
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/day10_List/QAsalSayi.java
@@ -0,0 +1,44 @@
+package day10_List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QAsalSayi {
+
+	public static void main(String[] args) {
+	
+		 /*
+	   100'den küçük asal sayýlarý yazdýrýnýz.
+	   
+	     */
+		List <Integer> asalSayilar=new ArrayList<>();
+		
+		int sayi=2;
+		int sayac=0;
+		
+		
+		while ( sayi < 100   ) {  // Kendisine bölünebilen sayýlar asal sayýdýr!! O yüzden sadece kendisine bölünecek
+			
+			// 2 3 4 5 6 7 8 9 ...  
+			for (int i = 2; i <= sayi; i++) {
+				
+				if (sayi%i==0) {    // For ile kendisine bölünebilenleri bulduk
+					sayac++;
+				}	
+			}
+			
+			
+			if (sayac==1) {
+					asalSayilar.add(sayi);	  // Kendisine bölündüyse
+				}
+				
+	
+
+			sayi++;
+			sayac=0;
+			
+	}	
+		
+		System.out.println(asalSayilar);
+	}
+}
